a85dfffbba8265e5241243d3ed984d11386adb5c
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2022-05-08 17:02:23 +0300

    Creating blog with admin panel. Creating CRUD for category, post, tag. Adding role, Jobs, queue.

---
+++ resources/views/personal/comment/index.blade.php
@@ -0,0 +1,70 @@
+@extends('personal.layouts.main')
+@section('content')
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Коментарии</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item active">Коментарии</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <div class="row">
+                    <div class="col-12"></div>
+                    <div class="card">
+                        <!-- /.card-header -->
+                        <div class="card-body table-responsive p-0">
+                            <table class="table table-head-fixed text-nowrap">
+                                <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Название</th>
+                                    <th colspan="2" class="text-center">Действие</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($comments as $comment)
+                                    <tr>
+                                        <td>{{ $comment->id}}</td>
+                                        <td>{{ $comment->message}}</td>
+                                        <td class="text-center"><a href="{{route('admin.comment.edit', $comment->id )}}"
+                                                                   class="text-success"> <i class="fa-solid fa-pen"></i></a></td>
+                                        <td class="text-center">
+                                            <form action="{{ route('personal.liked.delete', $comment->id) }}"
+                                                  method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="border-0 bg-transparent">
+                                                    <i class="fa-solid fa-trash text-danger" role="button"></i>
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+@endsection
